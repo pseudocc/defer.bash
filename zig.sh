5652e7c4e44723096e7e2582807622a086284ec6
pseudoc <atlas.yu@canonical.com>
2024-07-12 00:02:01 +0800

    style: use indent(space=2)

--- zig.sh
+++ zig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# vim: ts=4:et
+# vim: ts=2:et
 #
 # This is a bash library that provides the Zig-like `defer` and `errdefer`
 # functionalities. Please feel free to copy and paste this code into your
@@ -23,41 +23,41 @@ if [ -n "$DEBUG" ]; then set -x; fi
 _defer() { eval "$*"; }
 _errdefer() { if [ "$ZIG_EAX" -ne 0 ]; then eval "$*"; fi }
 _extract_trap() {
-    local trap_str=$1
-    local cb
-    cb=${trap_str#"trap -- '"}
-    cb=${cb%"' RETURN"}
-    echo "$cb"
+  local trap_str=$1
+  local cb
+  cb=${trap_str#"trap -- '"}
+  cb=${cb%"' RETURN"}
+  echo "$cb"
 }
 
 # shellcheck disable=SC2154
 alias return='if read -r rc; then
-    if [ -z "$(trap -p RETURN)" ]; then
-        return $rc;
-    else
-        ZIG_EAX=$rc;
-        return $rc;
-    fi
+  if [ -z "$(trap -p RETURN)" ]; then
+    return $rc;
+  else
+    ZIG_EAX=$rc;
+    return $rc;
+  fi
 fi <<<'
 
 # shellcheck disable=SC2154
 alias defer='if read -r cb; then
-    local prev_cb
-    prev_cb=$(_extract_trap "$(trap -p RETURN)")
-    if [ -z "$prev_cb" ]; then
-        trap "_defer $cb; trap - RETURN; ZIG_EAX=0" RETURN
-    else
-        trap "_defer $cb; $prev_cb" RETURN
-    fi
+  local prev_cb
+  prev_cb=$(_extract_trap "$(trap -p RETURN)")
+  if [ -z "$prev_cb" ]; then
+    trap "_defer $cb; trap - RETURN; ZIG_EAX=0" RETURN
+  else
+    trap "_defer $cb; $prev_cb" RETURN
+  fi
 fi <<<'
 
 # shellcheck disable=SC2154
 alias errdefer='if read -r cb; then
-    local prev_cb
-    prev_cb=$(_extract_trap "$(trap -p RETURN)")
-    if [ -z "$prev_cb" ]; then
-        trap "_errdefer ""$cb""; trap - RETURN; ZIG_EAX=0" RETURN
-    else
-        trap "_errdefer ""$cb""; $prev_cb" RETURN
-    fi
+  local prev_cb
+  prev_cb=$(_extract_trap "$(trap -p RETURN)")
+  if [ -z "$prev_cb" ]; then
+    trap "_errdefer ""$cb""; trap - RETURN; ZIG_EAX=0" RETURN
+  else
+    trap "_errdefer ""$cb""; $prev_cb" RETURN
+  fi
 fi <<<'
